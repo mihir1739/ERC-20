209d10e6f80239c68e2b04fc412ff9affdf79fa9
Mihir Kumar Singh <mihir39nr@gmail.com>
2022-08-15 19:49:31 +0530

    1st commit

---
+++ contract-26326c5637.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts@4.7.3/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts@4.7.3/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts@4.7.3/security/Pausable.sol";
+import "@openzeppelin/contracts@4.7.3/access/Ownable.sol";
+
+contract MyToken is ERC20, ERC20Burnable, Pausable, Ownable {
+    constructor() ERC20("MyToken", "MTK") {}
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount)
+        internal
+        whenNotPaused
+        override
+    {
+        super._beforeTokenTransfer(from, to, amount);
+    }
+}
